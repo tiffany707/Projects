09857baa7a50f038b9e817d781eadbc29e9ea2b3
tiffany707 <tiffanyokura7@gmail.com>
2026-01-03 02:09:34 -0700

    feat(todo): add model and initial routes

---
+++ todo-app/server/routes/todo.routes.js
@@ -0,0 +1,69 @@
+import express from "express"
+import ToDo from "../models/todo.model.js"
+
+const router = express.Router();
+
+//get all todos
+router.get("/", async (req,res)=>{
+    try{
+        const todos = await ToDo.find();
+        res.json(todos);
+    }
+    catch(error){
+        res.status(500).json({message:error.message});
+    }
+})
+
+//add new todo
+router.post("/", async (req, res) => {
+    const todo = new ToDo({
+        text: req.body.text
+    });
+    try{
+        const newToDo = await todo.save();
+        res.status(201);
+    }
+    catch(error){
+        res.status(400).json({message:error.message});
+    }
+})
+
+//Update todo text text and/or completed 
+router.put('/:id', async (req, res) => {
+    try{
+        const findToDo = await ToDo.findById(req.params.id);
+        if(! findToDo){
+            return res.status(404).json({message:"ToDo not found"});
+        }
+        
+        if(req.body.text !== undefined){
+            findToDo.text = req.body.text;
+        }
+
+        if(req.body.completed != undefined){
+            findToDo.completed = req.body.completed;
+        }
+
+        const updatedToDo = await findToDo.save();
+        res.json(updatedToDo);
+    }
+    catch(error){
+        res.status(400).json({message:error.message});
+    }
+
+})
+
+//delete the task
+router.delete("/:id", async (req, res) => {
+    try{
+        const todo = await ToDo.findByIdAndDelete(req.params.id);
+        res.json({message:"ToDo updated"})
+    }
+    catch(error){
+         return req.status(500).json({message:error.message})
+    }
+    
+   
+})
+
+export default router;
--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -3,12 +3,11 @@ dotenv.config(); // Move this to the top of the file
 
 import express from "express";
 import { connectDB } from "./config/db.js"; // Import this AFTER config
-
-
-
-dotenv.config();
+import todoRouter from "./routes/todo.routes.js"
 
 const app = express();
+
+app.use("/api/todos", todoRouter);
 
 app.get("/", (req,res) => {
     res.send("This is the main page");
